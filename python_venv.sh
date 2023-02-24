78f1cdd4a42259196ce4946c4cf60a9fe86879a4
fbrcode <fabio.bressler@gmail.com>
2023-02-24 17:02:56 -0300

    feat: add elastic search python lib

--- python_venv.sh
+++ python_venv.sh
@@ -4,6 +4,9 @@ source .venv/bin/activate
   # Python 3.10.9
 # pip install --upgrade pip
 # pip list -v
+# pip install -r requirements.txt
 # pip install "apache-airflow[celery]==2.5.1" --constraint "https://raw.githubusercontent.com/apache/airflow/constraints-2.5.1/constraints-3.10.txt"
 # pip install "apache-airflow[postgres]" 
 # pip install pandas
+# pip install elasticsearch
+# pip freeze > requirements.txt
